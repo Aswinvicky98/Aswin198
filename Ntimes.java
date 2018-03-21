b9935a4b77d740c60b4465ff01dfcadc20cf830d
Aswinvicky98 <36394890+Aswinvicky98@users.noreply.github.com>
2018-03-21 12:19:08 +0530

    Create Ntimes.java

---
+++ Ntimes.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Ntimes {
+
+    public static void main(String[] args) {
+
+    
+    	Scanner scan = new Scanner(System.in);
+    	System.out.println("Enter in no of times:");
+    	int n=scan.nextInt();
+    	scan.close();
+    	for (int i=1;i<=n;i++ ) 
+    	{
+            System.out.println("HELLO:");
+    	}
+    }
+}
